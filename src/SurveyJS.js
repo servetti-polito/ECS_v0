d486e8580f6538b6e432e914f7372f3148ad3562
MARTINA SAUGO s285326 <s285326@studenti.polito.it>
2022-05-21 13:16:43 +0200

    Introduced some new survey structures

--- src/SurveyJS.js
+++ src/SurveyJS.js
@@ -2,7 +2,8 @@ import 'survey-core/modern.min.css';
 import { Survey, Model } from 'survey-react-ui';
 import { StylesManager } from 'survey-core';
 import "./App.css";
-import * as surveyJSON from './resources/survey.json';
+//import * as surveyJSON from './resources/survey_vertical.json';
+import * as surveyJSON from './resources/survey_horizontal.json';
 
 StylesManager.applyTheme("modern");
 
