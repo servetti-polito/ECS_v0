b0f4ed04e6b3a11594f27c59134a970f04b67a6c
Martina Saugo <s285326@studenti.polito.it>
2022-07-18 15:17:02 +0200

    introduced cognito authentication (app level)

--- src/Page401.js
+++ src/Page401.js
@@ -5,7 +5,7 @@ function Page401(props)
             <div className="row h-75 align-items-center">
                 <div className="col-12">
                     <h1 class="text-center" id="err404">{props.ita ? "401: Non autorizzato" : "401: Unauthorized"}</h1><br/>
-                    <h1 class="text-center">{props.ita ? "Scansiona un url valido" : "Scan a valid url"}</h1>
+                    <h1 class="text-center">{props.ita ? "Scansiona un url valido da QR code" : "Scan a valid url from QR code"}</h1>
                 </div>
             </div>
         </div>
