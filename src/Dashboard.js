95fca00996a270a49be1dee94165de6ad22a18b9
Martina Saugo <s285326@studenti.polito.it>
2022-09-22 14:14:58 +0200

    test16-correct2

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -353,8 +353,9 @@ export default function Dashboard(props) {
 
         setInterval(()=>{
             console.log("in")
-            template.from=""+new Date()-5000;
-            template.to=""+new Date()-0;
+            template.from=(new Date()-5000).toString();
+            template.to=(new Date()-0).toString();
+            console.log("TEMPLATE",JSON.stringify(template))
             let xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState === 4 && this.status === 200) {
