a39b5d26b858d10b5016e0c951e74e9aaa03d9c6
Martina Saugo <s285326@studenti.polito.it>
2022-10-14 18:19:56 +0200

    ?

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,8 +1,8 @@
 import {CognitoUserPool} from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "us-east-1_EDjVEAFzD",
-    ClientId: "2aosd0oa4hkfv01b84c6tfah3p"
+    UserPoolId: "eu-west-3_rMtCV6Oho",
+    ClientId: "40gqtif2ukr91bom358nvtv8v3"
 }
 
 export default new CognitoUserPool(poolData);
