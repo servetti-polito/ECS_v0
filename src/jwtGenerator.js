506072b8f0b9e32029fe575acd91b19848d62557
Martina Saugo <s285326@studenti.polito.it>
2022-11-23 00:01:00 +0100

    moved jwt signature inside code

--- src/jwtGenerator.js
+++ src/jwtGenerator.js
@@ -1,7 +1,4 @@
-import {API} from "aws-amplify"
-//import jwt from "jsonwebtoken"
-//import * as privateKEY from "./resources/privateKey.txt"
-//export default async function jwtGenerator(username, deviceJwt){
+import * as jose from 'jose' //jwt
 export default async function jwtGenerator(username){
 
     /*let init = {
@@ -19,8 +16,8 @@ export default async function jwtGenerator(username){
                 exp:(Date.now()/1000)+3600
             }
         })
-        .catch(err=>{console.log(err); return null})*/
-/*    const signOptions = {
+        .catch(err=>{console.log(err); return null})
+    const signOptions = {
         issuer:  "prometeo.click",
         subject:  username,
         expiresIn:  "1h",
@@ -29,5 +26,46 @@ export default async function jwtGenerator(username){
     return jwt.sign({}, privateKEY, signOptions,()=>{
         console.log("err")
     });*/
-    return "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjkxNDMzNzQsImV4cCI6MjY2OTE0Njk3NCwiaXNzIjoicHJvbWV0ZW8uY2xpY2siLCJzdWIiOiJtYXJ0aW5hIn0.IBJzHu-01HwU3ihjWV3yIs1usxttpWyZprBFDg_qAFBDJQnDETXxI7nmX5q0VlNOlZOEoqMbqGgaEXMFNXc3lY2Qo8irrAP99IAq9c3QfJ7isUjBXZFQw4wbfLtp1KTOAtEG_k_EnTYueAHnUCI3RmefmfVDlndKBrIrBOsoO7tRAsxAs4rxVYa3shI1JCRAtU3eFHViCJz9pw-IMTWTgeZk5G9ot39STlYF59zet5iMwU9JGbbDSIRe5uU0jSCCUGObCHXVuNrjok8p7v91Yf1kSj5XFJBn0Gfghl5SryUccol6YhHMv22Ar3aclDw2JYwOleKlLCeyHOQQOcuS7A"
+
+    const alg = 'RS256'
+    const pkcs8 = `-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCexMb6+leTU8wC
+ldXxdUcphOOhlvgrNxJOGp0h4kZAR9VIyMMvSxtQwPPRJBfyqe+ug0SvrQhwh4xO
+vWYMwE7+o+4SCeKItu+4bnBppEmpU+FRd4QcM1b+D97uJCpOD4e8LU3oDkBO6Xne
+mjTQGjHVZbuKiKcmaYx4f+hcwp3U8E49zjyxnpknw4Y7C2GAs/OhPJkVoHJMZmsP
+sAjLaKtbxeyKTtG2GzuLOtWTm9kGX8J+t3nuF4ZS1m0QhXUmE9DAcv/ymxesQVfE
+wCWBpCOrFnphroIFgQYjx9vZIudm4k4fw3HmQIj0iaV93bY40AFKTSHCMJ71F41i
+63zfyelZAgMBAAECggEAVEgVcQoQD09+jyjHR8SAIFRBGniwCRDVEM0j+ObLcDgx
+ys94fdSbDYVAaReF9vWrHLLmdXOfdU0feHUmhUJZLOgM61p//R9PplJKLmpeCBtV
+ExAk+2ttlLUMHNLp+kJYRdau8q8OCFX8seSegJJHPgchRWtBgCyruxP7X7KYYn7N
+rD2agg94wKFVpt59HyWsmVz82aVWj5zeTL+z2WNNhi5Mwoj3qO2DXiKPLUeQuh+6
+iiwf9tgMejBGUOc4kbp/ApJcRELmA4SGF37wPX/Er4lRXY4kyVl/Ss4Y8isDAbr0
+lMH6Sd63qJNVJyLlPUUWkNSrc2X1QoPBWpv8Q+qgAQKBgQDt8SOc+BJ8uyWgm7WH
+yQe649SnJKqoWz98lr5cZqkP+Igt9mSO76MKUI+3bizu8yJw44wi8j/522bMpaJH
+smjx4Xn/B0pm3EhMp1wy54xA7wIR0QzwPCFubPRiZBdcHgZ4MYd/MdKIU66KY/W+
+wTeOio+rHko9px2RJ/ScLN/fWQKBgQCq0WsDGHimO3n8WABrSOSalbAwSr8R6kxX
+q+TSOPyaCZodhmCoGgm5rADsFKLGzRSyAJuZXUwJqUbMXj/AJIMk8eNzsQcedvnX
+S+rnQIL6oBLP+VhlCU0pkiQyxSY7WhAjG6V12RT/jyiuNSWN1Aee5nN6yjZYw+y9
+tdrow7MaAQKBgBKRMJA9I2QsTmFcBSxVWY8R3DQ2vkJo7GlScgRP1n1yjV3YzjwB
+Ix3Y734G0ObMfjGE8BiCAtACeOEbQ7CIkXvedmCaNwoTwmiI3r7yJpWikOzG/VV5
+u7ocMm3DFbeQeDzE0KfyQVV2Aja2WiTKewVadp+Ju41YDwyWq5XMQqPxAoGAC2fg
+VVyU8cSW6JqXrJI5cXj9Qrw69d9HKbN4m/DiLQbnG4GVgqP6ckNh7GGUXOHkt94C
+hcMNCLgc2hiQ+St29kiQf3EaoJYhKJcRIE/WbxWxK+HiXRy54kS31gwD+YKSAUYf
+Tjcoh8fvV3KFB2tY9us8Wr6V0meCr0gtTc2DcAECgYEAotwWfwkF3RIKhPejARZG
++1NlHJR7e1iMx4w48mGrdxbfX+84xuhcBm4Pe4dApJpQneZB20HBnopYQU3XjZs0
+W7jGUjqsFagKrwFIw0shbUjqOmwQndRQsHnnAYZPQoQMgHVe4jvq27GNwLQWeKRH
+W1gLl83nLUO0fSuejuxjMpw=
+-----END PRIVATE KEY-----`
+
+    const privateKey = await jose.importPKCS8(pkcs8, alg)
+    const jwt = await new jose.SignJWT({})
+        .setProtectedHeader({ alg })
+        .setIssuedAt()
+        .setIssuer('prometeo.click')
+        .setAudience(username)
+        .setExpirationTime('2h')
+        .sign(privateKey)
+    console.log("JOSE: "+jwt)
+    return jwt
+    //return "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjkxNDMzNzQsImV4cCI6MjY2OTE0Njk3NCwiaXNzIjoicHJvbWV0ZW8uY2xpY2siLCJzdWIiOiJtYXJ0aW5hIn0.IBJzHu-01HwU3ihjWV3yIs1usxttpWyZprBFDg_qAFBDJQnDETXxI7nmX5q0VlNOlZOEoqMbqGgaEXMFNXc3lY2Qo8irrAP99IAq9c3QfJ7isUjBXZFQw4wbfLtp1KTOAtEG_k_EnTYueAHnUCI3RmefmfVDlndKBrIrBOsoO7tRAsxAs4rxVYa3shI1JCRAtU3eFHViCJz9pw-IMTWTgeZk5G9ot39STlYF59zet5iMwU9JGbbDSIRe5uU0jSCCUGObCHXVuNrjok8p7v91Yf1kSj5XFJBn0Gfghl5SryUccol6YhHMv22Ar3aclDw2JYwOleKlLCeyHOQQOcuS7A"
 }
